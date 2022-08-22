e608b3d9f6c9b0e6e20d182e8038bfc7ec9be2eb
Nick_Munene <nickmunene101@gmail.com>
2022-08-22 09:41:17 +0300

    completed cart

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,10 +1,19 @@
-import './cartItem.css'
+import "./cartItem.css"
 
-function CartItem({id, image, title, price, quantity=0}) {
+import {
+  incrementQuantity,
+  decrementQuantity,
+  removeItem
+} from "../redux/cartSlice"
+
+import { useDispatch } from "react-redux"
+
+function CartItem({ id, image, title, price, quantity = 0 }) {
+  const dispatch = useDispatch()
 
   return (
     <div className="cartItem">
-      <img className="cartItem__image" src={image} alt='item'/>
+      <img className="cartItem__image" src={image} alt="item" />
 
       <div className="cartItem__info">
         <p className="cartItem__title">{title}</p>
@@ -12,12 +21,17 @@ function CartItem({id, image, title, price, quantity=0}) {
           <small>$</small>
           <strong>{price}</strong>
         </p>
-        <div className='cartItem__incrDec'>
-          <button>-</button>
+        <div className="cartItem__incrDec">
+          <button onClick={() => dispatch(decrementQuantity(id))}>-</button>
           <p>{quantity}</p>
-          <button>+</button>
+          <button onClick={() => dispatch(incrementQuantity(id))}>+</button>
         </div>
-        <button className='cartItem__removeButton'>Remove</button> 
+        <button
+          onClick={() => dispatch(removeItem(id))}
+          className="cartItem__removeButton"
+        >
+          Remove
+        </button>
       </div>
     </div>
   )
